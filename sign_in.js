8e4b7c95ff6f5604bd907facfb4577d22e54346c
Umeshkumar24 <101031787+Umeshkumar24@users.noreply.github.com>
2022-08-28 09:49:02 +0530

    done

--- sign_in.js
+++ sign_in.js
@@ -15,3 +15,38 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
+
+document.getElementById('reg-btn').addEventListener('click',function(){
+    document.getElementById("register-div").style.display = "inline";
+    document.getElementById("login-div").style.display="none";
+
+});
+document.getElementById('log-btn').addEventListener('click',function(){
+    document.getElementById("register-div").style.display = "none";
+    document.getElementById("login-div").style.display="inline";
+
+});
+
+document.getElementById("login-btn").addEventListener('click',function(){
+    const loginEmail=document.getElementById("login-email").value;
+    const loginPassword = document.getElementById("login-password").value;
+
+
+    createUserWithEmailAndPassword(auth,email,password)
+    .then((userCredential) =>{
+        const user = userCredential.user;
+        document.getElementById("result-box").style.display="inline";
+        document.getElementById("login-div").style.display="none";
+        document.getElementById("result").innerHTML="Welcome Back<br>"+loginEmail+"was login Successfully";
+
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        document.getElementById("result-box").style.display="inline";
+        document.getElementById("login-div").style.display="none";
+        document.getElementById("result").innerHTML="Sorry!"+ errorMessage;
+
+    });
+
+})
